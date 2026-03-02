2959caea3b416b688bd471635c5e03c0eb77faac
Lenny Ng'ang'a <codewithlennylen254@gmail.com>
2026-03-02 16:27:39 +0300

    esp32 firmware

--- esp32-firmware/labview_toggle_with_watchdog/src/main.cpp
+++ esp32-firmware/labview_toggle_with_watchdog/src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int ledPin = 2; // Internal LED
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+
+  digitalWrite(ledPin, HIGH);
+  delay(2000);
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Wait until LabVIEW sends us a command
+  if (Serial.available() > 0) {
+    
+    // Read the incoming character
+    char command = Serial.read();
+    
+    // Drain the rest of the buffer
+    while(Serial.available() > 0) { 
+      Serial.read(); 
+      delay(2); 
+    }
+    
+    // Execute and Respond
+    if (command == '1') {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("ACK_1"); // Send confirmation back to LabVIEW
+    } 
+    else if (command == '0') {
+      digitalWrite(ledPin, LOW);
+      Serial.println("ACK_0"); // Send confirmation
+    } 
+    else if (command == '?') {
+      Serial.println("ALIVE"); // Heartbeat response
+    }
+  }
 }
